fec8286a793e7cfd255905fc7110363b3cfb2795
Gustavo Manca <gustavo.manca@pixter.com.br>
2021-09-27 23:56:40 -0300

    refactor: group imports in index files

--- src/domain/test/mock-account.ts
+++ src/domain/test/mock-account.ts
@@ -1,5 +1,5 @@
 import { AuthenticationParams } from '@/domain/usecases/authentication'
-import { AccountModel } from '../models/account-model'
+import { AccountModel } from '../models'
 import faker from 'faker'
 
 export const mockAuthentication = (): AuthenticationParams => ({
