f577f58dbd40f9a83df6fab79f7913af5fbfbf59
Gustavo Manca <gustavo.mancaa@gmail.com>
2021-10-01 01:08:23 -0300

    feat: add app router

--- src/presentation/components/index.ts
+++ src/presentation/components/index.ts
@@ -4,3 +4,5 @@ export { default as Header } from './Header'
 export { default as Input } from './Input'
 export { default as Logo } from './Logo'
 export { default as Spinner } from './Spinner'
+
+export { default as Router } from './Router'
